2ffa1ed38eeb984548a98c3d4a5a4f077b65fdd0
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2023-08-31 08:16:38 -0400

    Done Bio Page and Working Progress of Shows Page

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,14 @@
+const bandShowEl = document.querySelector(".bandShows");
+
+const bandShowInfo = [
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+];
