0ea4d117014e3353dc8c107995c3b97aee4b9b60
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2023-09-04 21:47:06 -0400

    FINAL COMMIT: All Documentation Added and Final Checks Done!

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,3 +1,19 @@
+/*
+    Name: Anurag Bhattacharya
+    Project Name: BandSite Project - Sprint 2
+    Document Description: This is the Build Shows JavaScript File
+    
+    Diving Deeper:
+    - Unable to figure out Dynamic timestamp with Comments form
+    - For Comments Form, there is Form Validations
+    - If there are any errors:
+        - Prevents form from submitting
+        - Shows the error state as mentioned on the Style Guide
+    - If there are no errors:
+        - Clears any error states from form
+        - Submits the form
+*/
+//Band Shows Array
 const bandShowsArray = [
   {
     date: "Mon Sept 06 2021",
@@ -30,91 +46,94 @@ const bandShowsArray = [
     location: "San Francisco, CA",
   },
 ];
-
+//This Function Displays the Show Data
 function displayShows(bandShowsArray) {
+  //Getting the element to store the show data
   const bandShowsEl = document.querySelector(".bandShows");
-
+  //Band Show Data Main Title
   const bandShowsTitle = document.createElement("h2");
   bandShowsTitle.classList.add("bandShows__mainTitle");
   bandShowsTitle.innerText = "Shows";
   bandShowsEl.appendChild(bandShowsTitle);
-
+  //Band Show Data Header Wrapper
   const bandShowsHeaderWrapper = document.createElement("div");
   bandShowsHeaderWrapper.classList.add("bandShows__headerWrapper");
   bandShowsEl.appendChild(bandShowsHeaderWrapper);
-
+  //Band Show Header Content
   const bandShowsHeaderContent = document.createElement("div");
   bandShowsHeaderContent.classList.add("bandShows__headerContent");
   bandShowsHeaderWrapper.appendChild(bandShowsHeaderContent);
-
+  //Band Show Date Header
   const bandShowDateHeader = document.createElement("h3");
   bandShowDateHeader.classList.add("bandShows__headerContent-date");
   bandShowDateHeader.innerText = "DATE";
   bandShowsHeaderContent.appendChild(bandShowDateHeader);
-
+  //Band Show Venue Header
   const bandShowVenueHeader = document.createElement("h3");
   bandShowVenueHeader.classList.add("bandShows__headerContent-venue");
   bandShowVenueHeader.innerText = "VENUE";
   bandShowsHeaderContent.appendChild(bandShowVenueHeader);
-
+  //Band Show Location Header
   const bandShowLocationHeader = document.createElement("h3");
   bandShowLocationHeader.classList.add("bandShows__headerContent-location");
   bandShowLocationHeader.innerText = "LOCATION";
   bandShowsHeaderContent.appendChild(bandShowLocationHeader);
-
+  //Band Show Hidden Data - needed to format Table Headers
   const hiddenEl = document.createElement("span");
   hiddenEl.classList.add("bandShows__hidden");
   hiddenEl.innerText = ".";
   bandShowsHeaderContent.appendChild(hiddenEl);
 
+  //For Loop to go through each Band Shows Data in Array
   for (let key in bandShowsArray) {
+    //Band Shows Parent
     const bandShowsParent = document.createElement("div");
     bandShowsParent.classList.add("bandShows__showData");
     bandShowsHeaderWrapper.appendChild(bandShowsParent);
-
+    //Band Shows Content
     const bandShowsContent = document.createElement("div");
     bandShowsContent.classList.add("bandShows__showContent");
     bandShowsParent.appendChild(bandShowsContent);
-
+    //Band Shows Date Title
     const dateTitle = document.createElement("h4");
     dateTitle.classList.add("bandShows__dateTitle");
     dateTitle.innerText = "DATE";
     bandShowsContent.appendChild(dateTitle);
-
+    //Band Shows Date
     const date = document.createElement("h3");
     date.classList.add("bandShows__date");
     date.innerText = bandShowsArray[key]["date"];
     bandShowsContent.appendChild(date);
-
+    //Band Shows Venue Title
     const venueTitle = document.createElement("h4");
     venueTitle.classList.add("bandShows__venueTitle");
     venueTitle.innerText = "VENUE";
     bandShowsContent.appendChild(venueTitle);
-
+    //Band Shows Venue
     const venue = document.createElement("h3");
     venue.classList.add("bandShows__venue");
     venue.innerText = bandShowsArray[key]["venue"];
     bandShowsContent.appendChild(venue);
-
+    //Band Shows Location Title
     const locationTitle = document.createElement("h4");
     locationTitle.classList.add("bandShows__locationTitle");
     locationTitle.innerText = "LOCATION";
     bandShowsContent.appendChild(locationTitle);
-
+    //Band Shows Location
     const location = document.createElement("h3");
     location.classList.add("bandShows__location");
     location.innerText = bandShowsArray[key]["location"];
     bandShowsContent.appendChild(location);
-
+    //Band Shows Buy Tickets Button Wrapper
     const buyTicketsButtonWrapper = document.createElement("div");
     buyTicketsButtonWrapper.classList.add("bandShows__buttonWrapper");
     bandShowsContent.appendChild(buyTicketsButtonWrapper);
-
+    //Band Shows Buy Tickets Button
     const buyTicketsButton = document.createElement("button");
     buyTicketsButton.classList.add("bandShows__button");
     buyTicketsButton.innerText = "BUY TICKETS";
     buyTicketsButtonWrapper.appendChild(buyTicketsButton);
   }
 }
-
+//To Run displayShows Function
 displayShows(bandShowsArray);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,3 +1,20 @@
+/*
+    Name: Anurag Bhattacharya
+    Project Name: BandSite Project - Sprint 2
+    Document Description: This is the Index Page JavaScript File
+    
+    Diving Deeper:
+    - Unable to figure out Dynamic timestamp with Comments form
+    - For Comments Form, there is Form Validations
+    - If there are any errors:
+        - Prevents form from submitting
+        - Shows the error state as mentioned on the Style Guide
+    - If there are no errors:
+        - Clears any error states from form
+        - Submits the form
+*/
+
+//Comments List Array
 const commentsList = [
   {
     name: "Conner Walton",
@@ -19,34 +36,36 @@ const commentsList = [
   },
 ];
 
+//This function creates each comment article
 function createCommentCard(commentsList) {
+  //Image Wrapper
   const imageWrapperEl = document.createElement("div");
-  imageWrapperEl.classList.add("commentsPanel__avaterWrapper");
-
+  imageWrapperEl.classList.add("commentsPanel__avatarWrapper");
+  //Image
   const image = document.createElement("img");
-  image.classList.add("commentsPanel__avater");
-
+  image.classList.add("commentsPanel__avatar");
+  //Comment Article
   const cardEl = document.createElement("article");
   cardEl.classList.add("commentsPanel__comment");
-
+  //Comment Content
   const commentsContentEl = document.createElement("div");
   commentsContentEl.classList.add("commentsPanel__commentContent");
-
+  //Comment Header Content
   const headingContent = document.createElement("div");
   headingContent.classList.add("commentsPanel__headingContent");
-
+  //Comment Header - Name
   const heading = document.createElement("h3");
   heading.classList.add("commentsPanel__title");
   heading.innerText = commentsList.name;
-
+  //Comment - Date
   const date = document.createElement("p");
   date.classList.add("commentsPanel__date");
   date.innerText = commentsList.date;
-
+  //Comment - Comment
   const commentEl = document.createElement("span");
   commentEl.classList.add("commentsPanel__personComment");
   commentEl.innerText = commentsList.comment;
-
+  //Appending all elements to parent elements as needed
   cardEl.appendChild(imageWrapperEl);
   imageWrapperEl.appendChild(image);
   cardEl.appendChild(commentsContentEl);
@@ -54,62 +73,85 @@ function createCommentCard(commentsList) {
   headingContent.appendChild(heading);
   headingContent.appendChild(date);
   commentsContentEl.appendChild(commentEl);
-
+  //Returning the comment card for each comment
   return cardEl;
 }
-
+//This function renders the Comments Panel Content
 function renderComments() {
+  //Comments Panel Content
   const commentsEl = document.querySelector(".commentsPanel__content");
-
   commentsEl.innerHTML = "";
 
+  //For Loop through Comments Array to get Comments Data
   for (let i = 0; i < commentsList.length; i++) {
+    //Create a Comment Card for a Comment Data
     const card = createCommentCard(commentsList[i]);
+    //Append the Comment to the Array
     commentsEl.appendChild(card);
   }
+  //Get the Name and Comment Inputs
   let nameInput = document.getElementById("name");
   let commentInput = document.getElementById("comment");
+  //Empty them after rendering the comments panel data
   nameInput = "";
   commentInput = "";
 }
 
+//This function listens to the Submit Button on the Comments Form event
 function handleFormSubmit(event) {
+  //Prevents Form Submission Default Behaviour
   event.preventDefault();
+  //Gets the Name and Comment Input values
   let name = event.target.name.value;
   let comment = event.target.comment.value;
+  //Creating the Date in correct format
   let dateObject = new Date();
+  //Month
   let month = dateObject.getMonth() + 1;
+  //Day
   let day = dateObject.getDate();
+  //Year
   let year = dateObject.getFullYear();
+  //If statements to make sure to add a 0 in front of day/month value, if needed
   if (day < 10) day = "0" + day;
   if (month < 10) month = "0" + month;
+  //Correct Date Format
   let date = `${month}/${day}/${year}`;
+  //Form Validation Steps
+  //If Both Name and Comment Input empty, surround with red border
   if (name === "" && comment === "") {
     let nameInput = document.getElementById("name");
     let commentInput = document.getElementById("comment");
     nameInput.style.border = "1px solid #D22D2D";
     commentInput.style.border = "1px solid #D22D2D";
     return;
+    //If Name Input empty, surround with red border
   } else if (name === "") {
     let nameInput = document.getElementById("name");
     nameInput.style.border = "1px solid #D22D2D";
     return;
+    //If Comment Input empty, surround with red border
   } else if (comment === "") {
     let commentInput = document.getElementById("comment");
     commentInput.style.border = "1px solid #D22D2D";
     return;
   }
+  //Create the new comment
   const newComment = {
     name,
     comment,
     date,
   };
+  //Append the new comment to the end of the array
   commentsList.unshift(newComment);
+  //Call the renderComments Function
   renderComments();
+  //Reset the Comments Form upon submission
   commentsForm.reset();
 }
-
+//Get the Element for the Comment Form
 const commentsForm = document.getElementById("commentsForm__form");
+//Event Listen on submit button click on Comments Form
 commentsForm.addEventListener("submit", handleFormSubmit);
-
+//Call the renderComments Function
 renderComments();
