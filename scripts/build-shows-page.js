1d11ed05749871adc2deb01b3ed5f0adf76e8015
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2023-09-24 00:41:10 -0400

    FINAL COMMIT: All Documentation Added & Final Checks Completed!

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,93 +1,120 @@
 /*
-    Name: Anurag Bhattacharya
-    Project Name: BandSite Project - Sprint 2
-    Document Description: This is the Build Shows JavaScript File
-    
-    Diving Deeper:
-    - Unable to figure out Dynamic timestamp with Comments form
-    - For Comments Form, there is Form Validations
-    - If there are any errors:
-        - Prevents form from submitting
-        - Shows the error state as mentioned on the Style Guide
-    - If there are no errors:
-        - Clears any error states from form
-        - Submits the form
+  Name: 
+  Anurag Bhattacharya
+  Project: 
+  BandSite Project - Sprint 3
+  Description: 
+  This is the Build Shows Table JavaScript Functionalities
+  Diving Deeper Notes:
+  - All Diving Deeper From Sprint 1 Completed
+  - Form Validation Diving Deeper From Sprint 2 Completed
+  - Dynamic Timestamp Diving Deeper From Sprint 2 Not Completed
+  - All Diving Deeper From Sprint 3 Completed
 */
 
+//API Information - URL, api-key, endpoint
 let baseURL = "https://project-1-api.herokuapp.com";
 let api_key = "?api_key=7c2395bc-a9b1-4505-b5c5-0331ea11bff0";
 let showsEndpoint = "/showdates";
 let fullURL = `${baseURL}${showsEndpoint}${api_key}`;
 
+/**
+ * Displays Show Table Headers and Show Data
+ * @param {Array} response
+ */
 axios
   .get(`${fullURL}`)
   .then((response) => {
+    //Calls the function to display Show Data Headers on page
     displayShowHeaders();
+    /**
+     * Displays each Show Data Information
+     * @param {Array} response.data
+     */
     displayShowData(response.data);
   })
+  //To Catch any errors during the GET Request to API
   .catch((error) => {
     console.error("Fetch Error:", error);
   });
 
-//This Function Displays the Show Data
+/**
+ * Displays the Show Table Headers
+ */
 function displayShowHeaders() {
-  //Getting the element to store the show data
+  //Gets the Parent element for the Show Data Table
   const bandShowsEl = document.querySelector(".bandShows");
-  //Band Show Data Main Title
+
+  //Show Data Table Main Title
   const bandShowsTitle = document.createElement("h2");
   bandShowsTitle.classList.add("bandShows__mainTitle");
   bandShowsTitle.innerText = "Shows";
   bandShowsEl.appendChild(bandShowsTitle);
-  //Band Show Data Header Wrapper
+
+  //Show Data Table Wrapper
   const bandShowsHeaderWrapper = document.createElement("div");
   bandShowsHeaderWrapper.classList.add("bandShows__headerWrapper");
   bandShowsEl.appendChild(bandShowsHeaderWrapper);
-  //Band Show Header Content
+
+  //Show Data Table Content Wrapper
   const bandShowsHeaderContent = document.createElement("div");
   bandShowsHeaderContent.classList.add("bandShows__headerContent");
   bandShowsHeaderWrapper.appendChild(bandShowsHeaderContent);
-  //Band Show Date Header
+
+  //Show Data Table Header - DATE
   const bandShowDateHeader = document.createElement("h3");
   bandShowDateHeader.classList.add("bandShows__headerContent-date");
   bandShowDateHeader.innerText = "DATE";
   bandShowsHeaderContent.appendChild(bandShowDateHeader);
-  //Band Show Venue Header
+
+  //Show Data Table Header - VENUE
   const bandShowVenueHeader = document.createElement("h3");
   bandShowVenueHeader.classList.add("bandShows__headerContent-venue");
   bandShowVenueHeader.innerText = "VENUE";
   bandShowsHeaderContent.appendChild(bandShowVenueHeader);
-  //Band Show Location Header
+
+  //Show Data Table Header - LOCATION
   const bandShowLocationHeader = document.createElement("h3");
   bandShowLocationHeader.classList.add("bandShows__headerContent-location");
   bandShowLocationHeader.innerText = "LOCATION";
   bandShowsHeaderContent.appendChild(bandShowLocationHeader);
-  //Band Show Hidden Data - needed to format Table Headers
+
+  //Show Data Table Hidden Element in Header, used for formatting Header Titles
   const hiddenEl = document.createElement("span");
   hiddenEl.classList.add("bandShows__hidden");
   hiddenEl.innerText = ".";
   bandShowsHeaderContent.appendChild(hiddenEl);
 }
 
+/**
+ * Displays each Show Data on page
+ * @param {Array} data
+ */
 function displayShowData(data) {
+  //Gets the Parent element needed for the Shows Data
   let bandShowsHeaderWrapper = document.querySelector(
     ".bandShows__headerWrapper"
   );
-  //For Loop to go through each Band Shows Data in Array
+
+  //For loop to get the Key of each Show Data, used to find each specific information for each Show Data
   for (let key in data) {
-    //Band Shows Parent
+    //Gets the Parent element for the individual Shows Data
     const bandShowsParent = document.createElement("div");
     bandShowsParent.classList.add("bandShows__showData");
     bandShowsHeaderWrapper.appendChild(bandShowsParent);
-    //Band Shows Content
+
+    //Individual Shows Data Content
     const bandShowsContent = document.createElement("div");
     bandShowsContent.classList.add("bandShows__showContent");
     bandShowsParent.appendChild(bandShowsContent);
-    //Band Shows Date Title
+
+    //Individual Shows Data Title - DATE
     const dateTitle = document.createElement("h4");
     dateTitle.classList.add("bandShows__dateTitle");
     dateTitle.innerText = "DATE";
     bandShowsContent.appendChild(dateTitle);
-    //Band Shows Date
+
+    //Individual Shows Data - Date information
     const date = document.createElement("h3");
     date.classList.add("bandShows__date");
     let unixTimeStamp = data[key]["date"];
@@ -95,37 +122,50 @@ function displayShowData(data) {
     let dateString = showsDate.toDateString();
     date.innerText = dateString;
     bandShowsContent.appendChild(date);
-    //Band Shows Venue Title
+
+    //Individual Shows Data Title - VENUE
     const venueTitle = document.createElement("h4");
     venueTitle.classList.add("bandShows__venueTitle");
     venueTitle.innerText = "VENUE";
     bandShowsContent.appendChild(venueTitle);
-    //Band Shows Venue
+
+    //Individual Shows Data - Venue Information
     const venue = document.createElement("h3");
     venue.classList.add("bandShows__venue");
     venue.innerText = data[key]["place"];
     bandShowsContent.appendChild(venue);
-    //Band Shows Location Title
+
+    //Individual Shows Data Title - LOCATION
     const locationTitle = document.createElement("h4");
     locationTitle.classList.add("bandShows__locationTitle");
     locationTitle.innerText = "LOCATION";
     bandShowsContent.appendChild(locationTitle);
-    //Band Shows Location
+
+    //Indivudal Shows Data - Location Information
     const location = document.createElement("h3");
     location.classList.add("bandShows__location");
     location.innerText = data[key]["location"];
     bandShowsContent.appendChild(location);
-    //Band Shows Buy Tickets Button Wrapper
+
+    //Buy Tickets Button Wrapper
     const buyTicketsButtonWrapper = document.createElement("div");
     buyTicketsButtonWrapper.classList.add("bandShows__buttonWrapper");
     bandShowsContent.appendChild(buyTicketsButtonWrapper);
-    //Band Shows Buy Tickets Button
+
+    //Buy Tickets Button
     const buyTicketsButton = document.createElement("button");
     buyTicketsButton.classList.add("bandShows__button");
     buyTicketsButton.innerText = "BUY TICKETS";
     buyTicketsButtonWrapper.appendChild(buyTicketsButton);
   }
+
+  //Constant Variable to find all the rows of the Shows Data Table
   const showDataRow = document.querySelectorAll(".bandShows__showData");
+
+  /**
+   * Applying Selected Row Styles to the individual Shows Data row, that is clicked on (selected)
+   * @param {Div Element} show
+   */
   showDataRow.forEach((show) => {
     show.addEventListener("click", (event) => {
       showDataRow.forEach((show) => {
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,59 +1,80 @@
 /*
-    Name: Anurag Bhattacharya
-    Project Name: BandSite Project - Sprint 2
-    Document Description: This is the Index Page JavaScript File
-    
-    Diving Deeper:
-    - Unable to figure out Dynamic timestamp with Comments form
-    - For Comments Form, there is Form Validations
-    - If there are any errors:
-        - Prevents form from submitting
-        - Shows the error state as mentioned on the Style Guide
-    - If there are no errors:
-        - Clears any error states from form
-        - Submits the form
+  Name: 
+  Anurag Bhattacharya
+  Project: 
+  BandSite Project - Sprint 3
+  Description: 
+  This is the Index Page / Band Bio Page JavaScript Functionalities
+  Diving Deeper Notes:
+  - All Diving Deeper From Sprint 1 Completed
+  - Form Validation Diving Deeper From Sprint 2 Completed
+  - Dynamic Timestamp Diving Deeper From Sprint 2 Not Completed
+  - All Diving Deeper From Sprint 3 Completed
 */
 
+//API Information - URL, api-key, endpoint
 let baseURL = "https://project-1-api.herokuapp.com";
 let api_key = "?api_key=38d065a7-e5a0-442f-bd28-22ddfbc7f490";
 let commentsEndpoint = "/comments";
 let fullURL = `${baseURL}${commentsEndpoint}${api_key}`;
 
+/**
+ * Sorts the Comments Data in ascending order based on timestamp
+ * @param {Array} response
+ */
 const sortComment = () => {
   axios
     .get(`${fullURL}`)
     .then((response) => {
+      /**
+       * Compares and Sorts individual Comments Data based on timestamp
+       * @param {Object} a
+       * @param {Object} b
+       */
       let sortedData = response.data.sort((a, b) => {
         return b.timestamp - a.timestamp;
       });
+      //Calls the Function to render the comments on the page
       renderComments(sortedData);
     })
+    //Catches any errors, if errors found during GET Request to API
     .catch((error) => {
       console.error("Fetch Error:", error);
     });
 };
 
-//This function creates each comment article
+/**
+ * Displays each individual comments data information
+ * @param {Array} data
+ * @returns Individual Comments Card Element
+ */
 const displayComment = (data) => {
-  //Image Wrapper
+  //Individual Comment Avatar Image Wrapper
   const imageWrapperEl = document.createElement("div");
   imageWrapperEl.classList.add("commentsPanel__avatarWrapper");
-  //Image
+
+  //Individual Comment Avatar Image
   const image = document.createElement("img");
   image.classList.add("commentsPanel__avatar");
-  //Comment Article
+
+  //Individual Comment Parent Article
   const cardEl = document.createElement("article");
   cardEl.classList.add("commentsPanel__comment");
-  //Comment Content
+
+  //Individual Comment Content Wrapper
   const commentsContentEl = document.createElement("div");
   commentsContentEl.classList.add("commentsPanel__commentContent");
-  //Comment Header Content
+
+  //Individual Comment Heading Content Wrapper
   const headingContent = document.createElement("div");
   headingContent.classList.add("commentsPanel__headingContent");
-  //Comment Header - Name
+
+  //Individual Comment Header Title - Name
   const heading = document.createElement("h3");
   heading.classList.add("commentsPanel__title");
   heading.innerText = data.name;
+
+  //Individual Comment Information - Date
   let unixTimeStamp = data.timestamp;
   let date = new Date(unixTimeStamp);
   let month = date.getMonth() + 1;
@@ -62,146 +83,248 @@ const displayComment = (data) => {
   if (day < 10) day = "0" + day;
   if (month < 10) month = "0" + month;
   let dateString = `${month}/${day}/${year}`;
-  //Comment - Date
   const dateEl = document.createElement("p");
   dateEl.classList.add("commentsPanel__date");
   dateEl.innerText = dateString;
-  //Comment - Comment
+
+  //Individual Comment Information - Comment
   const commentEl = document.createElement("span");
   commentEl.classList.add("commentsPanel__personComment");
   commentEl.innerText = data.comment;
 
+  //Likes, # of Likes and Delete Button Icons Wrapper
   const crudIcons = document.createElement("section");
   crudIcons.classList.add("commentsPanel__crudIcons");
+
+  //Like Button Wrapper
   const likeButtonWrapper = document.createElement("div");
   likeButtonWrapper.classList.add("commentsPanel__likeButtonWrapper");
+
+  //Like Button Image
   const likeButton = document.createElement("img");
   likeButton.classList.add("commentsPanel__likeButton");
+  //Assigns ID of Like Button element to the ID of the individual comment
   likeButton.id = data.id;
   likeButton.src = "../assets/icons/icon-like.svg";
-  crudIcons.appendChild(likeButtonWrapper);
-  likeButtonWrapper.appendChild(likeButton);
+
+  //# of Likes Information
   const commentLikes = document.createElement("span");
   commentLikes.classList.add("commentsPanel__commentLikes");
   commentLikes.innerText = data.likes;
+
+  //Appends the LikeButton and # of Likes Information to Parent Element
+  crudIcons.appendChild(likeButtonWrapper);
+  likeButtonWrapper.appendChild(likeButton);
   likeButtonWrapper.appendChild(commentLikes);
 
+  /**
+   * Click Event Lister Function to update the # of Likes for an individual Comment
+   * @param {String} event
+   */
   likeButton.addEventListener("click", (event) => {
+    //Gets the ID of the individual comment
     let likeId = event.target.id;
+    //PUT Request URL
     let fullURLPUT = `${baseURL}${commentsEndpoint}/${likeId}/like/${api_key}`;
     axios
       .put(`${fullURLPUT}`)
+      /**
+       * Updates the # of Likes Information by 1, as a User clicks the Like Button
+       * @param {Array} response
+       */
       .then((response) => {
         commentLikes.innerText = response.data.likes;
       })
+      //To catch any errors in the PUT Request to API
       .catch((error) => {
         console.log(error);
       });
   });
 
+  //Delete Button Wrapper
   const deleteButtonWrapper = document.createElement("div");
   deleteButtonWrapper.classList.add("commentsPanel__deleteButtonWrapper");
-  crudIcons.appendChild(deleteButtonWrapper);
+
+  //Delete Button Image
   const deleteButton = document.createElement("img");
   deleteButton.classList.add("commentsPanel__deleteButton");
+
+  //Assigns ID of Delete Button to the ID of the individual comment
   deleteButton.id = data.id;
   deleteButton.src = "../assets/icons/icon-delete.svg";
-  deleteButtonWrapper.appendChild(deleteButton);
 
+  //Appends Delete Button Image to Delete Button Wrapper
+  deleteButtonWrapper.appendChild(deleteButton);
+  //Appends Delete Button Wrapper to Parent element
+  crudIcons.appendChild(deleteButtonWrapper);
+
+  /**
+   * Click Event Listener to delete the individual comment that is clicked
+   * @param {String} event
+   */
   deleteButton.addEventListener("click", (event) => {
+    //Gets the Individual Comment ID
     let deleteId = event.target.id;
+    //Full DELETE Request to API
     let fullURLDELETE = `${baseURL}${commentsEndpoint}/${deleteId}${api_key}`;
+
+    //Pop-up message to confirm User wants to delete selected individual comment
     let confirmDelete = confirm("Are you sure you want to delete?");
+
+    //If true, User wants to delete selected individual comment, go ahead with the DELETE Request to API
     if (confirmDelete) {
       axios
         .delete(`${fullURLDELETE}`)
+        /**
+         * Deletes the individual comment and then sort latest Comments Array by calling the Sorting Function
+         * @param {Array} response
+         */
         .then((response) => {
+          //Calls the sortComment Function to sort the latest Comments by timestamp
           sortComment();
         })
+        //To catch any errors on the DELETE Request on API
         .catch((error) => {
           console.log(error);
         });
     }
   });
-  //Appending all elements to parent elements as needed
+
+  //Appending Comments Image Wrapper to Parent element
   cardEl.appendChild(imageWrapperEl);
+
+  //Appends Comments Image to Parent element
   imageWrapperEl.appendChild(image);
+
+  //Appends Comments Content Element to Parent element
   cardEl.appendChild(commentsContentEl);
+
+  //Appends Comments Header Content to Parent element
   commentsContentEl.appendChild(headingContent);
+
+  //Appends Header Content to Parent element
   headingContent.appendChild(heading);
   headingContent.appendChild(dateEl);
+
+  //Appends Full Comment Element to Parent Element
   commentsContentEl.appendChild(commentEl);
+
+  //Appends CRUD Icons Wrapper to Parent Element
   commentsContentEl.appendChild(crudIcons);
-  //Returning the comment card for each comment
+
+  //Returns Individual Comments Card Element
   return cardEl;
 };
 
-//This function renders the Comments Panel Content
+/**
+ * Renders the Full Comments Panel
+ * @param {Array} data
+ */
 const renderComments = (data) => {
-  //Comments Panel Content
+  //Comments Panel Content Wrapper
   const commentsEl = document.querySelector(".commentsPanel__content");
+  //On Page Load, empty the Parent element HTML
   commentsEl.innerHTML = "";
+
+  /**
+   * A For Each Loop to get each individual Comments
+   * @param {Array} comment
+   */
   data.forEach((comment) => {
+    //Gets the Individual Comment Card
     const card = displayComment(comment);
+    //Appends the Individual Comments Card to Parent element
     commentsEl.appendChild(card);
   });
-  //Get the Name and Comment Inputs
+
+  //Names and Comment Input from Form
   let nameInput = document.getElementById("name");
   let commentInput = document.getElementById("comment");
-  //Empty them after rendering the comments panel data
+
+  //Once, Comments are rendered on page, Empty the Name and Comments Input on the Comments Form
   nameInput = "";
   commentInput = "";
 };
+
+//Calling sortComment Function on page load, sorts and renders the Comments presented in API on page load
 sortComment();
 
-//This function listens to the Submit Button on the Comments Form event
+/**
+ * Comments Form Submit Form Functionality, appends Created Comment to API with POST Request
+ * @param {String} event
+ */
 function handleFormSubmit(event) {
+  //Prevents event default behavior, in this case, on Form Submit Click event, Page will not refresh
   event.preventDefault();
+
+  //Gets the Name and Comment values from User inputs on Name and Comment Input Fields
   let name = event.target.name.value;
   let comment = event.target.comment.value;
+
+  //Gets the Name and Comment Input Fields of Comments Form
   let nameInput = document.getElementById("name");
   let commentInput = document.getElementById("comment");
-  //Form Validation Steps
-  //If Both Name and Comment Input empty, surround with red border
+
+  //Form Validation, making sure, there are no errors prior to functionalities on Form submission
+  //If both Name and Comments Input is empty, both Input Elements will have an Error styles
   if (name === "" && comment === "") {
     nameInput.classList.remove("default-border");
     commentInput.classList.remove("default-border");
     nameInput.classList.add("error-border");
     commentInput.classList.add("error-border");
     return;
-    //If Name Input empty, surround with red border
+
+    //Else if Name Input is empty, Name Input Element will have Error styles
   } else if (name === "") {
     nameInput.classList.remove("default-border");
     nameInput.classList.add("error-border");
     return;
-    //If Comment Input empty, surround with red border
+
+    //Else if Comments Input is empty, Comments Input Element will have Error styles
   } else if (comment === "") {
     commentInput.classList.remove("default-border");
     commentInput.classList.add("error-border");
     return;
+
+    //Else No errors, Default Styles applied to Both Input Elements
   } else {
     nameInput.classList.remove("error-border");
     nameInput.classList.add("default-border");
     commentInput.classList.remove("error-border");
     commentInput.classList.add("default-border");
   }
+
+  //Creates a new Comment Data Object
   const newCommentData = {
     name: name,
     comment: comment,
   };
 
+  /**
+   * This Function calls POST Request on API, and then adds to the API,
+   * calls sortComment Function to sort Comments and render onto page with other helper functions
+   * @param {Array} response
+   */
   axios.post(`${fullURL}`, newCommentData).then((response) => {
+    //Calls sortComment function, which sorts the Latest Comments array by timestamp
     sortComment();
   });
 
+  //Removes Error Styles to Input Elements and on page load, Default Styling applied to Input Elements
   nameInput.classList.remove("error-border");
   commentInput.classList.remove("error-border");
   nameInput.classList.add("default-border");
   commentInput.classList.add("default-border");
+
+  //Resets the Comment Form
   commentsForm.reset();
 }
 
-//Get the Element for the Comment Form
+//Gets the Comment Form Element
 const commentsForm = document.getElementById("commentsForm__form");
-//Event Listen on submit button click on Comments Form
+
+/*
+Event Listener on Comments Form Element, calls handleFormSubmit, 
+which handles the addition of a new Comments to the Comments Array in API
+*/
 commentsForm.addEventListener("submit", handleFormSubmit);
